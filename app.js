c48f506fd454394af774ac940a2cf33bb59c3296
Jerome <jeromelesne@gmail.com>
2024-02-13 22:31:38 +0100

    basic setup

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.listen(port, () => console.log(`App listening to port ${port}`));
